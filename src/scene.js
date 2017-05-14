49c961e913bad4c57ba6b193cdabf70b8f0b7002
Sönke Hahn <soenkehahn@gmail.com>
2017-05-13 22:10:35 -0400

    allow to move right

--- src/scene.js
+++ src/scene.js
@@ -5,21 +5,31 @@ type Pos = {|
   y: number,
 |}
 
+type Direction
+  = 'right'
+
 type Player  = {|
   position: Pos,
 |}
 
 type Scene = {|
   player: Player,
+  step: Direction => void,
 |}
 
 export function mkScene(): Scene {
-  return {
+  const scene = {
     player: {
       position: {
         x: 0,
         y: 0,
+      },
+    },
+    step: direction => {
+      if (direction === 'right') {
+        scene.player.position.x += 1
       }
-    }
+    },
   }
+  return scene
 }
--- tests/scene_test.js
+++ tests/scene_test.js
@@ -6,10 +6,18 @@ import {expect} from 'chai'
 import {mkScene} from '../src/scene'
 
 describe('scene', () => {
-  
+
   it('contains the player', () => {
     const scene = mkScene()
     expect(scene.player.position).to.eql({x: 0, y: 0})
+  })
+
+  describe('step', () => {
+    it('allows to move the character right', () => {
+      const scene = mkScene()
+      scene.step('right')
+      expect(scene.player.position).to.eql({x: 1, y: 0})
+    })
   })
 
 })
