99c707fdc8cc835038d68df897c42b0ee5ebdf92
Sönke Hahn <soenkehahn@gmail.com>
2017-05-14 15:38:08 -0400

    small refactoring

--- src/ui/scene.js
+++ src/ui/scene.js
@@ -29,15 +29,7 @@ export class SceneComponent extends React.Component<void, {}, {| scene: Scene |}
   }
 }
 
-class Render extends React.Component<void, {scene: Scene}, {}> {
-
-  state = {}
-
-  constructor(props: {scene: Scene}) {
-    super(props)
-    this.state = mkScene()
-  }
-
+class Render extends React.Component<void, {scene: Scene}, void> {
   render() {
     const objects = this.props.scene.toObjects()
     return <svg viewBox="-10 -10 20 20" width="800" height="600" xmlns="http://www.w3.org/2000/svg">
