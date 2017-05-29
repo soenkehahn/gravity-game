2dbeb16c88f81d89a9af491fbd2e1cbf623f5b03
Sönke Hahn <soenkehahn@gmail.com>
2017-05-29 18:38:10 -0400

    add more levels

--- src/real_levels.js
+++ src/real_levels.js
@@ -2,6 +2,7 @@
 
 import type {Scene} from './scene'
 import {Planet, EndPlanet} from './scene'
+import {scale} from './objects'
 
 export type RealLevel = number
 
@@ -41,6 +42,26 @@ const levels: Array<(Scene) => void> = [
     ]
     scene.endPlanets = [
       new EndPlanet({x: -unit, y: -unit}, 1),
+    ]
+  },
+
+  (scene) => {
+    scene.player.position = scale({x: -3, y: 16}, 0.5)
+    scene.planets = [
+      new Planet(scale({x: -3, y: 16}, 0.5), 0.1),
+    ]
+    scene.endPlanets = [
+      new EndPlanet(scale({x: 3, y: -16}, 0.5), 1),
+    ]
+  },
+
+  (scene) => {
+    scene.player.position = scale({x: 16, y: -3}, 0.75)
+    scene.planets = [
+      new Planet(scale({x: 16, y: -3}, 0.75), 0.1),
+    ]
+    scene.endPlanets = [
+      new EndPlanet(scale({x: -16, y: 3}, 0.75), 1),
     ]
   },
 
