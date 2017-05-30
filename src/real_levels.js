32919c4e0145723b9650735d6c178cb35a3d6808
Sönke Hahn <soenkehahn@gmail.com>
2017-05-29 22:22:12 -0400

    convert level collection to array

--- src/real_levels.js
+++ src/real_levels.js
@@ -7,34 +7,54 @@ import {scale} from './objects'
 export type RealLevel = number
 
 export function getLevel(scene: Scene, level: RealLevel): void {
-  const createLevel = levels[level.toString()]
+  const createLevel = levels[level - 1]
   if (createLevel) {
     createLevel(scene)
   }
 }
 
-const levels: {[string]: ((Scene) => void)} = {
-  '1': (scene) => {
+const levels: Array<Scene => void> = [
+  (scene) => {
     scene.player.position = {x: -5, y: 0}
     scene.planets = [
-      new Planet({x: -5, y: 0}, 0.2),
+      new Planet({x: -5, y: 0}, 0.1),
     ]
     scene.endPlanets = [
       new EndPlanet({x: 5, y: 0}, 1),
     ]
   },
 
-  '2': (scene) => {
+  (scene) => {
     scene.player.position = {x: 0, y: -5}
     scene.planets = [
-      new Planet({x: 0, y: -5}, 0.2),
+      new Planet({x: 0, y: -5}, 0.1),
     ]
     scene.endPlanets = [
       new EndPlanet({x: 0, y: 5}, 1),
     ]
   },
 
-  '3': (scene) => {
+  (scene) => {
+    scene.player.position = {x: 5, y: 0}
+    scene.planets = [
+      new Planet({x: 5, y: 0}, 0.2),
+    ]
+    scene.endPlanets = [
+      new EndPlanet({x: -5, y: 0}, 1),
+    ]
+  },
+
+  (scene) => {
+    scene.player.position = {x: 0, y: 5}
+    scene.planets = [
+      new Planet({x: 0, y: 5}, 0.2),
+    ]
+    scene.endPlanets = [
+      new EndPlanet({x: 0, y: -5}, 1),
+    ]
+  },
+
+  (scene) => {
     let unit = Math.sqrt(Math.pow(10.0, 2.0) / 2.0) / 2.0
     scene.player.position = {x: unit, y: unit}
     scene.planets = [
@@ -45,7 +65,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '4': (scene) => {
+  (scene) => {
     scene.player.position = scale({x: -3, y: 16}, 0.5)
     scene.planets = [
       new Planet(scale({x: -3, y: 16}, 0.5), 0.2),
@@ -55,7 +75,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '5': (scene) => {
+  (scene) => {
     scene.player.position = scale({x: 16, y: -3}, 0.75)
     scene.planets = [
       new Planet(scale({x: 16, y: -3}, 0.75), 0.2),
@@ -65,7 +85,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '6': (scene) => {
+  (scene) => {
     scene.player.position = scale({x: -5, y: 5}, 0.75)
     scene.planets = [
       new Planet(scale({x: -5, y: 5}, 0.75), 0.2),
@@ -76,7 +96,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '7': (scene) => {
+  (scene) => {
     scene.player.position = scale({x: -10, y: 5}, 0.75)
     scene.planets = [
       new Planet(scale({x: -10, y: 5}, 0.75), 0.2),
@@ -88,7 +108,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '8': (scene) => {
+  (scene) => {
     scene.player.position = scale({x: -10, y: 10}, 0.75)
     scene.planets = [
       new Planet(scale({x: -10, y: 10}, 0.75), 0.2),
@@ -101,7 +121,20 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '9': (scene) => {
+  (scene) => {
+    scene.player.position = scale({x: -10, y: 10}, 0.75)
+    scene.planets = [
+      new Planet(scale({x: -10, y: 10}, 0.75), 0.2),
+      new Planet(scale({x: -10, y: 0}, 0.75), 0.2),
+      new Planet(scale({x: 0, y: 0}, 0.75), 0.2),
+      new Planet(scale({x: 0, y: -10}, 0.75), 1),
+    ]
+    scene.endPlanets = [
+      new EndPlanet(scale({x: 10, y: -10}, 0.75), 1),
+    ]
+  },
+
+  (scene) => {
     scene.player.position = {x: -10, y: 0}
     scene.planets = [
       new Planet({x: -10, y: 0}, 0.1),
@@ -112,7 +145,7 @@ const levels: {[string]: ((Scene) => void)} = {
     ]
   },
 
-  '10': (scene) => {
+  (scene) => {
     scene.player.position = {x: -10, y: 0}
     scene.planets = [
       new Planet({x: -10, y: 0}, 0.1),
@@ -122,4 +155,4 @@ const levels: {[string]: ((Scene) => void)} = {
       new EndPlanet({x: 10, y: 0}, 1),
     ]
   },
-}
+]
