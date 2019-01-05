9265418dea2512cb6de2ecc433d261d9013bda13
Sönke Hahn <soenkehahn@gmail.com>
2019-01-05 03:35:18 -0500

    update flow

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import ReactDOM from "react-dom";
 import { SceneComponent } from "./ui/scene";
 
 const root = document.getElementById("root");
-ReactDOM.render(<SceneComponent startLevel={1} />, root);
+if (root) {
+  ReactDOM.render(<SceneComponent startLevel={1} />, root);
+} else {
+  throw new Error("root not found");
+}
